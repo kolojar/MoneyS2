111b001ecd56ee2dbefe63c290a718bdb1207e0e
LukasJarolim <lukasjarolim01@seznam.cz>
2026-09-09 12:50:58 +0300

    Complete login #14

---
+++ admin/login.php
@@ -0,0 +1,72 @@
+<?php
+    require __DIR__ . "/../assets/config.php";
+    session_start();
+
+    //Handle POST
+    if (isset($_POST["password"])) {
+        if($_POST["password"] == $_SERVER["adminPassword"]) {
+            http_response_code(200);
+            $_SESSION["isAdmin"] = true;
+            echo "ok";
+            die();
+        }
+        http_response_code(403);
+        echo "Invalid password";
+        die();
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Login to Admin</title>
+        <link rel="stylesheet" href="../formWebScripts/css/formStyle.css" />
+        <meta name="form-icons-main-db" content="../formWebScripts/formIcons.json" />
+    </head>
+    <body class="formBackground" form-box-holder>
+        <form-box>
+            <p class="formHeader">Login to Admin</p>
+            <form-input type="password" tabindex="1" label="Password" id="password" placeholder="Your password"></form-input>
+            <div class="formButtonBoxHolder formCenter">
+                <button id="send" tabindex="2" class="formButton formOkColor">Continue</button>
+            </div>
+        </form-box>
+    </body>
+    <script type="module" src="./formWebScripts/js/formScript.js"></script>
+    <script type="module">
+        import { SendPOSTDataToServerAsync } from "../formWebScripts/js/serverComunication.js";
+        import { SendToast, SetWaitStatusForms } from "../formWebScripts/js/formScript.js";
+        import { FormDialogManager } from "../formWebScripts/js/formDialogScript.js";
+        import { GlobalLanguageManager } from "../formWebScripts/js/formScript.js";
+
+        //GlobalLanguageManager.ChangeLanguage("en");
+        const dialogManager = new FormDialogManager();
+        document.getElementById("send").addEventListener("click", async () => {
+            SetWaitStatusForms("Sending data to server, please wait...");
+            const data = new FormData();
+            data.append("password", document.getElementById("password").value);
+
+            const [ok, res] = await SendPOSTDataToServerAsync("./login.php", data);
+
+            if (ok) {
+                if (res == "ok") {
+                    window.location.href = "./admin.php";
+                } else {
+                    SendToast("Server responce", res, "error");
+                    setTimeout(async () => {
+                        await dialogManager.ShowAlertAsync("Login to Admin", "Comunication with server failed, please try it again later.");
+                        window.location.reload();
+                    }, 1000);
+                }
+            } else {
+                SendToast("Server responce", res, "error");
+                setTimeout(async () => {
+                    await dialogManager.ShowAlertAsync("Login to Admin", "Invalid password, please try it again.");
+                    window.location.reload();
+                }, 1000);
+            }
+        });
+    </script>
+</html>
--- assets/config.php
+++ assets/config.php
@@ -1,12 +1,16 @@
 <?php
-// Přístupové údaje k databázi (Localhost)
+//Admin password
+$_SERVER["adminPassword"] = "admin";
+
+// DB login info
 $servername = "127.0.0.1:3307";
 $username = 'root';
 $password = "root";
 $dbname = "moneys2";
 
+//Connect
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
 if ($conn->connect_error) {
-    die("Chyba při připojování k databázi: " . $conn->connect_error);
+    die("Error connecting to DB: " . $conn->connect_error);
 }
