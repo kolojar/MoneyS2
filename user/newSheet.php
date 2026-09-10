3809140e7c76e51204c79744b00a48c9c6d13f3d
matkolo1 <matej.kor@email.cz>
2026-09-10 17:15:22 +0300

    newSheet.php completed for #3

--- user/newSheet.php
+++ user/newSheet.php
@@ -1,13 +1,33 @@
 <?php
-    /** @var \mysqli $conn */
-    require __DIR__ . "/../assets/config.php";
-    session_start();
+/** @var \mysqli $conn */
+require __DIR__ . "/../assets/config.php";
+session_start();
 
-    //Handle POST
-    if (isset($_POST["name"])) {
-        //TODO: LOGIC HERE
-        $stmt = $conn->prepare()
+//Handle POST
+if (isset($_POST["name"], $_POST["password"], $_POST["people"])) {
+    $query =
+        "CREATE TABLE `" .
+        $_POST["name"] .
+        "` (`id` INT AUTO_INCREMENT PRIMARY KEY, `when` DATETIME DEFAULT CURRENT_TIMESTAMP, `where` VARCHAR(255) NOT NULL, `who` TINYINT NOT NULL, `name` VARCHAR(255) NOT NULL, `cnt` TINYINT NOT NULL, `price` FLOAT NOT NULL, `link` INT";
+    
+    $people = explode(";", $_POST["people"]);
+        
+    foreach ($people as $person) {
+        $query .= ", `" . $person . "` FLOAT NOT NULL";
     }
+    
+    $query .= ") ENGINE=INNODB;";
+    //print($query);
+    if (!$conn->query($query)) {
+        echo "nah";
+        die();
+    } else {
+        $conn->query("INSERT INTO `tables` (`name`, `password`) VALUES ('".$_POST["name"]."', '".$_POST["password"]."');");
+        http_response_code(200);
+        echo "success";
+        die();
+    }
+}
 ?>
 
 <!doctype html>
@@ -25,8 +45,9 @@
             <p class="formHeader">Create new sheet</p>
             <form-input type="text" tabindex="1" label="Name" id="name" placeholder="Sheet name" minlength=1></form-input>
             <form-input type="password" tabindex="1" label="Password" id="password" placeholder="Password (optional)"></form-input>
+            <form-input type="text" tabindex="1" label="People" id="people" placeholder="john doe;jane doe;..." minlength=1></form-input>            
             <div class="formButtonBoxHolder formCenter">
-                <button id="send" tabindex="2" class="formButton formOkColor">Create</button>
+                <button id="send" tabindex="3" class="formButton formOkColor">Create</button>
             </div>
             <form-status-message></form-status-message>
         </form-box>
@@ -39,11 +60,15 @@
         import { GlobalLanguageManager } from "../formWebScripts/js/formScript.js";
 
         //GlobalLanguageManager.ChangeLanguage("en");
+        let nameCnt = 2;
         const dialogManager = new FormDialogManager();
         document.getElementById("send").addEventListener("click", async () => {
             SetWaitStatusForms("Sending data to server, please wait...");
             const data = new FormData();
+            data.append("name", document.getElementById("name").value.replaceAll(" ", "_"));
             data.append("password", document.getElementById("password").value);
+
+            data.append("people", document.getElementById("people").value.replaceAll(" ", "_"))
 
             const [ok, res] = await SendPOSTDataToServerAsync("./newSheet.php", data);
 
