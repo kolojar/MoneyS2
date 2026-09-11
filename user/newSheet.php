6b8a8625e4e88ad8b3ff60ab21536b259c2cea6c
LukasJarolim <lukasjarolim01@seznam.cz>
2026-09-11 15:58:29 +0300

    Fixed creating new table

--- user/newSheet.php
+++ user/newSheet.php
@@ -2,31 +2,47 @@
 /** @var \mysqli $conn */
 require __DIR__ . "/../assets/config.php";
 session_start();
+$LETTERS = "0123456789abcdefghijklmnopqstuvwxyzABCDEFGHIJLKMNOPQERSTUVWXYZ";
 
 //Handle POST
 if (isset($_POST["name"], $_POST["password"], $_POST["people"])) {
-    $query =
-        "CREATE TABLE `" .
-        $_POST["name"] .
-        "` (`id` INT AUTO_INCREMENT PRIMARY KEY, `when` DATETIME DEFAULT CURRENT_TIMESTAMP, `where` VARCHAR(255) NOT NULL, `who` TINYINT NOT NULL, `name` VARCHAR(255) NOT NULL, `cnt` TINYINT NOT NULL, `price` FLOAT NOT NULL, `link` INT";
-    
-    $people = explode(";", $_POST["people"]);
-        
-    foreach ($people as $person) {
-        $query .= ", `" . $person . "` FLOAT NOT NULL";
+    //Create new table evidence
+    $name = $_POST["name"];
+    $passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $people = $_POST["people"];
+    $stmt = $conn->prepare("INSERT INTO `_tables` (`name`, `password`,`persons`) VALUES (?,?,?);");
+    if (!$stmt->bind_param("sss", $name, $passwordHash, $people) || !$stmt->execute()) {
+        http_response_code(500);
+        echo "nah";
+        die();
     }
-    
+    $id = $stmt->insert_id;
+    $idCounter = $id;
+    $stmt->close();
+
+    //Encode table name
+    $nameEncoded = "";
+    do {
+        $nameEncoded = $LETTERS[$idCounter % 62] . $nameEncoded;
+        $idCounter = intdiv($$idCounter, 62);
+    } while ($idCounter > 0);
+
+    //Create table
+    $query = "CREATE TABLE `" . $nameEncoded . "` (`id` INT AUTO_INCREMENT PRIMARY KEY, `when` DATETIME DEFAULT CURRENT_TIMESTAMP, `where` VARCHAR(255) NOT NULL, `who` TINYINT NOT NULL, `name` VARCHAR(255) NOT NULL, `cnt` TINYINT NOT NULL, `price` FLOAT NOT NULL, `link` INT";
+    for ($i = 0; $i < count(explode(";", $people)); $i++) {
+        $query .= ", ` p" . $i . "` DECIMAL NOT NULL";
+    }
+
     $query .= ") ENGINE=INNODB;";
     //print($query);
     if (!$conn->query($query)) {
+        http_response_code(500);
         echo "nah";
         die();
-    } else {
-        $conn->query("INSERT INTO `tables` (`name`, `password`) VALUES ('".$_POST["name"]."', '".$_POST["password"]."');");
-        http_response_code(200);
-        echo "success";
-        die();
     }
+    http_response_code(200);
+    echo $id;
+    die();
 }
 ?>
 
@@ -45,7 +61,7 @@ if (isset($_POST["name"], $_POST["password"], $_POST["people"])) {
             <p class="formHeader">Create new sheet</p>
             <form-input type="text" tabindex="1" label="Name" id="name" placeholder="Sheet name" minlength=1></form-input>
             <form-input type="password" tabindex="1" label="Password" id="password" placeholder="Password (optional)"></form-input>
-            <form-input type="text" tabindex="1" label="People" id="people" placeholder="john doe;jane doe;..." minlength=1></form-input>            
+            <form-input type="text" tabindex="1" label="People" id="people" placeholder="john doe;jane doe;..." minlength=1></form-input>
             <div class="formButtonBoxHolder formCenter">
                 <button id="send" tabindex="3" class="formButton formOkColor">Create</button>
             </div>
@@ -65,27 +81,18 @@ if (isset($_POST["name"], $_POST["password"], $_POST["people"])) {
         document.getElementById("send").addEventListener("click", async () => {
             SetWaitStatusForms("Sending data to server, please wait...");
             const data = new FormData();
-            data.append("name", document.getElementById("name").value.replaceAll(" ", "_"));
+            data.append("name", document.getElementById("name").value);
             data.append("password", document.getElementById("password").value);
-
-            data.append("people", document.getElementById("people").value.replaceAll(" ", "_"))
+            data.append("people", document.getElementById("people").value)
 
             const [ok, res] = await SendPOSTDataToServerAsync("./newSheet.php", data);
 
             if (ok) {
-                if (res == "ok") {
-                    window.location.href = "./sheet.php?name="+ encodeURIComponent(document.getElementById("password").value);
-                } else {
-                    SendToast("Server responce", res, "error");
-                    setTimeout(async () => {
-                        await dialogManager.ShowAlertAsync("Login to Admin", "Comunication with server failed, please try it again later.");
-                        window.location.reload();
-                    }, 1000);
-                }
+                window.location.href = "./sheet.php?id="+ res;
             } else {
                 SendToast("Server responce", res, "error");
                 setTimeout(async () => {
-                    await dialogManager.ShowAlertAsync("Login to Admin", "Invalid password, please try it again.");
+                    await dialogManager.ShowAlertAsync("Login to Admin", "Failed creating sheet, please try it again.");
                     window.location.reload();
                 }, 1000);
             }
